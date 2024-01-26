53a7a2227e9ccd855b3be44717d242c12ab06377
nazwabilbina <nazwabilbina2006@gmail.com>
2024-01-26 11:49:10 +0700

    first commit

---
+++ configg.php
@@ -0,0 +1,16 @@
+<?php
+//variabel inisialisasi
+$servername= "localhost";
+$username = "root";
+$password = "";
+$database= "dbperpus";
+
+//create connection
+$link = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$link) {
+    die("KONEKSI GAGAL: " .mysqli_connect_error());
+  }
+  echo "KONEKSI SUKSES";
+?>
